a983d1284a8520ed3fde8054b5ac9602556faef1
⌯ 𝐙ᴇᴡsᴛᴀ ×͜× <Zewsta0@gmail.com>
2024-11-07 18:44:07 +0300

    Update resume.py

--- KlasikMuzik/plugins/admins/resume.py
+++ KlasikMuzik/plugins/admins/resume.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2024 by Klasik_Help @ Github, < https://github.com/TheTeamKlasik >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Klasik © Yukki.
-
-""""
-TheTeamKlasik is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Klasik <https://github.com/TheTeamKlasik>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -20,7 +8,7 @@ from KlasikMuzik.core.call import Klasik
 from KlasikMuzik.utils.database import is_Muzik_playing, Muzik_on
 from KlasikMuzik.utils.decorators import AdminRightsCheck
 
-# Commands
+# Komut
 RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 

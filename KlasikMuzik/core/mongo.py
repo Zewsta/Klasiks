5a5fa009eb84697f5a2afdedad17ca08133106a8
⌯ 𝐙ᴇᴡsᴛᴀ ×͜× <Zewsta0@gmail.com>
2024-11-07 18:02:59 +0300

    Update mongo.py

--- KlasikMuzik/core/mongo.py
+++ KlasikMuzik/core/mongo.py
@@ -1,29 +1,18 @@
-# Copyright (C) 2024 by Klasik_Help @ Github, < https://github.com/TheTeamKlasik >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Klasik © Yukki.
-
-""""
-TheTeamKlasik is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Klasik <https://github.com/TheTeamKlasik>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("Connecting to your Mongo Database...")
+LOGGER(__name__).info("Mongo Veritabanınıza Bağlanılıyor...‌‌")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Klasik
-    LOGGER(__name__).info("Connected to your Mongo Database.")
+    LOGGER(__name__).info("Mongo Veritabanınıza Bağlanıldı‌‌.")
 except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    LOGGER(__name__).error("Mongo Veritabanınıza Bağlanılamadı.‌‌")
     exit()
 
-## Database For Broadcast Subscription By Team Klasik
+## Klasik Müzik Reklam Aboneliği İçin Veritabanı
 MONGODB_CLI = AsyncIOMotorClient(MONGO_DB_URI)
 db = MONGODB_CLI["subscriptions"]

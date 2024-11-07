49cb2ff12e6cb285323b4264b1a3c9230671dddc
⌯ 𝐙ᴇᴡsᴛᴀ ×͜× <Zewsta0@gmail.com>
2024-11-07 18:23:52 +0300

    Update Soundcloud.py

--- KlasikMuzik/platforms/Soundcloud.py
+++ KlasikMuzik/platforms/Soundcloud.py
@@ -1,14 +1,3 @@
-# Copyright (C) 2024 by Klasik_Help @ Github, < https://github.com/TheTeamKlasik >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Klasik © Yukki.
-
-""""
-TheTeamKlasik is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Klasik <https://github.com/TheTeamKlasik>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 import re
 from os import path
 

3a083e77e1c5b225fd573398a77463c7a357916a
⌯ 𝐙ᴇᴡsᴛᴀ ×͜× <Zewsta0@gmail.com>
2024-11-07 17:31:22 +0300

    Update bot.py

--- KlasikMuzik/core/bot.py
+++ KlasikMuzik/core/bot.py
@@ -1,21 +1,50 @@
-# Copyright (C) 2024 by Klasik_Help @ Github, < https://github.com/TheTeamKlasik >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Klasik © Yukki.
-
-""""
-TheTeamKlasik is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Klasik <https://github.com/TheTeamKlasik>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 import sys
 
 from pyrogram import Client
 import config
 from ..logging import LOGGER
 from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import BotCommand, BotCommandScopeAllPrivateChats, BotCommandScopeAllGroupChats
+
+
+
+private_commands = [
+    BotCommand("start", "🎧 Botu başlatır"),
+    BotCommand("yardim", "📖 Yardım menüsünü gösterir"),
+]
+
+
+group_commands = [
+    BotCommand("oynat", "🔼 Müziği oynatır"),
+    BotCommand("voynat", "📹 Videoyu oynatır"),
+    BotCommand("atla", "⏭️ Sonraki Parçaya Geçer"),
+    BotCommand("duraklat", "⏸️ Çalan Parçayı Durdurur"),
+    BotCommand("devam", "▶️ Çalan Parçayı Devam Ettirir"),
+    BotCommand("son", "⏹️ Çalan Parçayı Kapatır"),
+    BotCommand("karistir", "🔀 Çalan Parçayı Karıştırır"),
+    BotCommand("dongu", "🔄 Çalan Parçayı Tekrarlar"),
+    BotCommand("sira", "📖 Çalma Listelerini Gösterir"),
+    BotCommand("ilerisar", "⏩ Parçayı İleri Sarar"),
+    BotCommand("gerisar", "⏪ Parçayı Geri Sarar"),
+    BotCommand("playlist", "📖 Çalma Listenizi Gösterir"),
+    BotCommand("bul", "📩 Seçtiğiniz Parçayı İndirir"),
+    BotCommand("ayarlar", "⚙️ Bot Ayarlarını Gösterir"),
+    BotCommand("restart", "🔃 Botu Yeniden Başlatır"),
+    BotCommand("reload", "❤️‍🔥 Yönetici Önbelleğini Günceller"),
+    BotCommand("id", "🆔 Kullanıcı ID'sini Verir"),
+    BotCommand("hava", "🌦️ Hava Durumunu Gösterir"),
+    BotCommand("cevir", "📚 Yabancı Kelimeleri Türkçeye Çevirir"),
+    
+]
+
+async def set_commands(client):
+    
+    await client.set_bot_commands(private_commands, scope=BotCommandScopeAllPrivateChats())
+    
+    
+    await client.set_bot_commands(group_commands, scope=BotCommandScopeAllGroupChats())
+
+
 
 
 class KlasikBot(Client):
@@ -29,7 +58,7 @@ class KlasikBot(Client):
             max_concurrent_transmissions=4,
             workers=50,
         )
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"Bot Başlatılıyor...")
 
     async def start(self):
         await super().start()
@@ -39,19 +68,22 @@ class KlasikBot(Client):
         self.mention = get_me.mention
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
+                config.LOG_GROUP_ID, "» **Klasik Müzik Bot Başarıyla Başlatıldı** ✨"
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "Bot, günlük grubuna erişemedi. Botunuzu günlük grubunuza eklediğinizden ve yönetici olduğundan emin olun!"
             )
             sys.exit()
+
+        await set_commands(self)
+
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+            LOGGER(__name__).error("Lütfen Bota Log Grubunda Yetki Verin")
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MuzikBot Started as {self.name}")
+        LOGGER(__name__).info(f"{self.name} Olarak Başlatıldı")

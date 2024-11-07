4c99125719d5e474c9cf5ee9265f88986ebb53b7
⌯ 𝐙ᴇᴡsᴛᴀ ×͜× <Zewsta0@gmail.com>
2024-11-07 19:35:32 +0300

    Update paidsub.py

--- KlasikMuzik/plugins/modules/paidsub.py
+++ KlasikMuzik/plugins/modules/paidsub.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2024 by Klasik_Help @ Github, < https://github.com/TheTeamKlasik >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Klasik © Yukki.
-
-""""
-TheTeamKlasik is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Klasik <https://github.com/TheTeamKlasik>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 import asyncio
 from KlasikMuzik import app
 from pyrogram import Client, filters
@@ -20,4 +8,4 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from KlasikMuzik.utils.database import get_served_users, get_served_chats
 
 
-OWNER_ID = 6174058850
+OWNER_ID = 8061949128
